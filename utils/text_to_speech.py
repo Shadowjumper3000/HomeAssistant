24cacbf15a4789e2a315e82ef0b801f18d25c640
Shadowjumper3000 <96634974+Shadowjumper3000@users.noreply.github.com>
2025-01-04 14:15:44 +0100

    elevenlabs audio functional API KEY in .env

--- utils/text_to_speech.py
+++ utils/text_to_speech.py
@@ -1,12 +1,18 @@
-import pyttsx3
+import os
+from dotenv import load_dotenv
+import elevenlabs
+
+load_dotenv()
+
+ELEVEN_LABS_API_KEY = os.getenv("ELEVENLABS_APIKEY")
+FFMPEG_PATH = os.getenv("FFMPEG_PATH")
+
+os.environ["PATH"] += os.pathsep + FFMPEG_PATH
 
 
 def output_text(text):
     try:
-        engine = pyttsx3.init()
-        engine.say(text)
-        engine.runAndWait()
-        with open("data/output.txt", "w") as f:
-            f.write(text + "\n")
+        audio = elevenlabs.generate(text, voice="Alice", api_key=ELEVEN_LABS_API_KEY)
+        elevenlabs.play(audio, use_ffmpeg=False)
     except Exception as e:
         print(f"Error in output_text: {e}")
